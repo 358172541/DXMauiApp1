7493fe8f177285656378e28e2e4bf124f8f9bf31
358172541 <358172541@qq.com>
2023-06-21 17:08:58 +0800

    .

--- DXMauiApp1/Services/ContactService.cs
+++ DXMauiApp1/Services/ContactService.cs
@@ -1,7 +1,23 @@
-﻿namespace DXMauiApp1.Services
+﻿using System.Net.Http.Headers;
+
+namespace DXMauiApp1.Services
 {
     public static class ContactService
     {
+        private static HttpClient CreateHttpClient()
+        {
+            var httpClient = new HttpClient();
 
+            httpClient.BaseAddress = new Uri("https://api.ydwl168.com");
+
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var accessToken = Preferences.Get("AccessToken", string.Empty);
+
+            if (!string.IsNullOrEmpty(accessToken))
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            return httpClient;
+        }
     }
 }
