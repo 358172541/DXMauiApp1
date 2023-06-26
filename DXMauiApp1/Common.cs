89264c61f7033b6f217bf9e7737afef4ce7d15ba
358172541 <358172541@qq.com>
2023-06-26 10:29:07 +0800

    .

--- DXMauiApp1/Common.cs
+++ DXMauiApp1/Common.cs
@@ -1,4 +1,6 @@
 ﻿using DevExpress.Maui.Editors;
+using System.Collections;
+using System.Reflection;
 
 namespace DXMauiApp1
 {
@@ -20,6 +22,41 @@ namespace DXMauiApp1
                 textEditBase.HasError = false;
                 return true;
             }
+        }
+
+        public static string QueryStringify<T>(T t) // experimental method
+        {
+            var properties = typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => x.GetValue(t) != null)
+                .ToDictionary(k => k.Name, v => v.GetValue(t));
+
+            var items = new List<string>();
+
+            foreach (var property in properties)
+            {
+                var key = char.ToLower(property.Key[0]) + property.Key[1..];
+
+                var value = property.Value;
+
+                if (value.GetType().IsGenericType &&
+                    value.GetType().GetGenericTypeDefinition() == typeof(List<>)) // limits List<>
+                {
+                    var index = 0;
+
+                    foreach (var item in (IEnumerable)value)
+                    {
+                        items.Add($"{key}[{index}]={item}");
+                        index++;
+                    }
+                }
+                else
+                {
+                    items.Add($"{key}={value}");
+                }
+            }
+
+            return string.Join("&", items);
         }
     }
 }
